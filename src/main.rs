1ad43d3eaf90bf84979660ed62c2baae171525d6
s-kybound <abadkyriel@gmail.com>
2024-03-29 15:33:35 +0800

    enable support for messages and commands

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,78 @@
 // This bot throws a dice on each incoming message.
 
-use teloxide::prelude::*;
+use teloxide::{prelude::*, utils::command::BotCommands};
 use rustrict::CensorStr;
+
+// define commands
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "blame a user")]
+    Blame(String),
+}
+
+async fn handle_commands(
+    bot: Bot,
+    msg: Message,
+    cmd: Command
+) -> Result<(), teloxide::RequestError> {
+    let text = match cmd {
+        Command::Blame(str) => format!("You are to blame, {}!", str),
+        Command::Help => Command::descriptions().to_string(),
+    };
+
+    bot.send_message(msg.chat.id, text).await?;
+
+    Ok(())
+}
+
+async fn handle_message(bot: Bot, msg: Message) -> ResponseResult<()> {
+    // get text from the message
+    let text = msg.text().unwrap();
+
+    // check for profanity
+    let inappropriate: bool = text.is_inappropriate();
+
+    if inappropriate {
+        // send a message when inappropriate language is detected
+        bot.send_message(msg.chat.id, "Please don't use inappropriate language!").await?;
+    } else {
+        // compliment the user on their good behavior
+        bot.send_message(msg.chat.id, "You are a good person!").await?;
+    }
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
+    run().await;
+}
+
+async fn run() {
+    // initialize the bot
     pretty_env_logger::init();
-    log::info!("Starting throw dice bot...");
+    log::info!("Starting profanity bot...");
 
-    let bot = Bot::new("YOUR_BOT_TOKEN");
+    let bot = Bot::from_env();
 
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        // check whether the message is inappropriate
-        let inappropriate: bool = msg.text().unwrap().is_inappropriate();
-        if inappropriate {
-            bot.send_message(msg.chat.id, "Please don't use inappropriate language!").await?;
-            return Ok(());
-        }
-        Ok(())
-    })
+    // set up the dispatcher
+    Dispatcher::builder(
+        bot,
+        Update::filter_message()
+            .branch(
+                // for commands
+                dptree::entry()
+                    .filter_command::<Command>()
+                    .endpoint(handle_commands))
+            .branch(
+                // for everything else
+                dptree::entry()
+                    .endpoint(handle_message))
+    )
+    .build()
+    .dispatch()
     .await;
 }
